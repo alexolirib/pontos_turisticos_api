76555ed867054805e651b9836710d165cb5a9c92
Alexandre Oliveira Ribeiro <alexolirib@gmail.com>
2019-10-06 16:07:01 -0300

    fazendo a modelagem de atracoes, avaliacoes, comentarios, ponto_turistico

---
+++ comentarios/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Comentario(models.Model):
+    #se deletar usuario, todos comentários irá ser deletado tbm
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentatio = models.TextField()
+    #auto_now_add cadastrar automatico data atual
+    data = models.DateTimeField(auto_now_add=True)
+    aprovao = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.usuario.username
---
+++ ponto_turistico/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from ponto_turistico.models import PontoTuristico
+
+admin.site.register(PontoTuristico)
+
