1e99c9a5c2f573476be37ca53fde3d72c64bfadb
Alexandre Oliveira Ribeiro <alexolirib@gmail.com>
2019-10-13 14:16:15 -0300

    implementando a serialização do ponto turísticos com os seus relacionamentos

--- ponto_turistico/api/serializers.py
+++ ponto_turistico/api/serializers.py
@@ -1,10 +1,31 @@
-
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
-
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 from ponto_turistico.models import PontoTuristico
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id',
+                  'nome',
+                  'descricao',
+                  'aprovado',
+                  'foto',
+                  'atracoes',
+                  'comentarios',
+                  'avaliacoes',
+                  'endereco',
+                  'descricao_completa',
+                  'descricao_completa2'
+        )
+
+    def get_descricao_completa(self, obj):
+        #pode pegar atributos do objeto que nem estão sendo usado
+        #nos fields
+        return '%s - %s' % (obj.nome, obj.descricao)
